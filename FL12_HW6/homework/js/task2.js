cb505432d72fdb3b05d84cab66003e9c815a1513
Oksana Kryshtop <oksana.krishtop@gmail.com>
2019-12-24 20:41:59 +0200

    HomeWork6

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -2,27 +2,26 @@ let a = prompt('Type first length:');
 let b = prompt('Type second length:');
 let c = prompt('Type third length:');
 
-if (isNaN(a) || isNaN(b) || isNaN(c) ||
-    a === '' || b === '' || c === '' ||
-    a.includes(' ') || b.includes(' ') || c.includes(' ')
-) {
-    alert('Input values should be ONLY numbers')
+if (isNaN(a) || isNaN(b) || isNaN(c)
+  || a === '' || b === '' || c === ''
+  || a.includes(' ') || b.includes(' ') || c.includes(' ')) {
+  alert('Input values should be ONLY numbers');
 } else {
-    a = parseFloat(a);
-    b = parseFloat(b);
-    c = parseFloat(c);
+  a = parseFloat(a);
+  b = parseFloat(b);
+  c = parseFloat(c);
 
-    if (a <= 0 || b <= 0 || c <= 0) {
-        alert('A triangle must have 3 sides with a positive definite length')
-    } else if (a <= b + c && b <= a + c && c <= b + c) {
-        if (a === b && a === c) {
-            console.log('Equilateral triangle')
-        } else if (a === b || a === c || b === c) {
-            console.log('Isosceles triangle')
-        } else {
-            console.log('Scalene triangle')
-        }
+  if (a <= 0 || b <= 0 || c <= 0) {
+    alert('A triangle must have 3 sides with a positive definite length');
+  } else if (a <= b + c && b <= a + c && c <= b + c) {
+    if (a === b && a === c) {
+      console.log('Equilateral triangle');
+    } else if (a === b || a === c || b === c) {
+      console.log('Isosceles triangle');
     } else {
-        alert('Triangle doesn’t exist')
+      console.log('Scalene triangle');
     }
+  } else {
+    alert('Triangle doesn’t exist');
+  }
 }
